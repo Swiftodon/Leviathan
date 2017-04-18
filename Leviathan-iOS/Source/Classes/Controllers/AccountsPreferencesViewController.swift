5fe3a0940e3a468833a245eb7e44cc582ab630b8
Bonk, Thomas <thomas.bonk@sap.com>
2017-04-18 12:09:38 +0200

    UI for creating and editing accounts

---
+++ Leviathan-iOS/Source/Classes/Controllers/AccountsPreferencesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AccountsPreferencesViewController.swift
+//  Leviathan
+//
+//  Created by Bonk, Thomas on 18.04.17.
+//
+//
+
+import UIKit
+
+class AccountsPreferencesViewController: UIViewController {
+
+    
+
+}
