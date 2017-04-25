4167e1fd2eef2feb28fab332613245825fd33e49
Marcus Kida <marcus@kida.io>
2017-04-25 13:37:48 +1000

    Implement RxSwift AccountSubject on Settings

--- Common/Classes/Model/Settings.swift
+++ Common/Classes/Model/Settings.swift
@@ -7,62 +7,54 @@
 //
 
 import Foundation
+import RxSwift
 
 fileprivate extension String {
-    
-    static let activeAccount         = "activeAccount"
-    static let activeAccountServer   = "activeAccountServer"
+    static let activeAccount = "activeAccount"
+    static let activeAccountServer = "activeAccountServer"
     static let activeAccountUsername = "activeAccountUsername"
 }
 
 @objc class Settings: NSObject {
     
     // MARK: - Private Properties
-    
-    private let defaultValues:  [String:Any] = [
+    private let defaultValues: [String: Any] = [
         String.activeAccountServer: "",
         String.activeAccountUsername: ""
     ]
     private let userDefaults  = UserDefaults.standard
-    
+    public let accountSubject = PublishSubject<Account>()
     
     // MARK: - Initialization
-    
     override init() {
         super.init()
-        
-        self.userDefaults.register(defaults: defaultValues)
+        userDefaults.register(defaults: defaultValues)
     }
     
     
     // MARK: - Settings
-    
-    @objc var activeAccount: Account? {
+    var activeAccount: Account? {
         get {
-            
-            let server = self.userDefaults.string(forKey: String.activeAccountServer)
-            let username = self.userDefaults.string(forKey: String.activeAccountUsername)
-            let accountController = Globals.injectionContainer.resolve(AccountController.self)
-            let account = accountController?.find(server!, username!)
-            
-            return account
+            guard let server = userDefaults.string(forKey: String.activeAccountServer) else {
+                return nil
+            }
+            guard let username = userDefaults.string(forKey: String.activeAccountUsername) else {
+                return nil
+            }
+            guard let accountController = Globals.injectionContainer.resolve(AccountController.self) else {
+                return nil
+            }
+            return accountController.find(server, username)
         }
         set {
-            
-            self.willChangeValue(forKey: String.activeAccount)
-            
-            self.userDefaults.willChangeValue(forKey: String.activeAccountServer)
-            self.userDefaults.set(newValue?.server, forKey: String.activeAccountServer)
-            self.userDefaults.didChangeValue(forKey: String.activeAccountServer)
-            
-            self.userDefaults.willChangeValue(forKey: String.activeAccountUsername)
-            self.userDefaults.set(newValue?.username, forKey: String.activeAccountUsername)
-            self.userDefaults.didChangeValue(forKey: String.activeAccountUsername)
-            
-            self.didChangeValue(forKey: String.activeAccount)
-            
-            self.userDefaults.synchronize()
+            guard let newValue = newValue else {
+                // TODO: Remove account if set to `nil` ???
+                return
+            }
+            userDefaults.set(newValue.server, forKey: String.activeAccountServer)
+            userDefaults.set(newValue.username, forKey: String.activeAccountUsername)
+            userDefaults.synchronize()
+            accountSubject.onNext(newValue)
         }
     }
-
 }
