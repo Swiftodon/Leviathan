9114cdd142c88d244cd5c5d0e26a7844cbbc6dac
Thomas Bonk <thomas@meandmymac.de>
2022-10-31 10:24:46 +0100

    Start of migration to SwiftUI

---
+++ Leviathan/Sources/Application/LeviathanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeviathanApp.swift
+//  Leviathan
+//
+//  Created by Thomas Bonk on 31.10.22.
+//
+
+import SwiftUI
+
+@main
+struct LeviathanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Leviathan/Sources/Views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Leviathan
+//
+//  Created by Thomas Bonk on 31.10.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
